dc1ac78010b53acbc2d66e6ebed061653de8ae8c
igorsoares123 <48934452+igorsoares123@users.noreply.github.com>
2019-10-14 23:15:19 -0300

    Add files via upload

---
+++ campeonato/src/main/java/com/projeto/campeonato/model/Time.java
@@ -0,0 +1,44 @@
+package com.projeto.campeonato.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Time{
+	
+	
+	
+	@Id
+	@GeneratedValue
+	
+	private Long id;
+	private String tecnico;
+	private String time;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTecnico() {
+		return tecnico;
+	}
+	
+	public void setTecnico(String tecnico) {
+		this.tecnico = tecnico;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+	
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+}
